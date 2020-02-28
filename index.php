fe4b9a0d562289aec8fff07a6f911b4dd67e3282
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-02-28 00:33:44 -0500

    added a page that poppulates from the file cars.json. I select only 10 cars and the ones that are not sold

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require("vendor/autoload.php");
 
 //Instantiate F3
 $f3 = Base::instance();
-
+$f3->set("div",array(0,1,2,3,4,5,6,7,8,9));
 
 //Define a default route
 $f3->route("GET /", function () {
