9f376a21edff6a8215829c2a86df9f35a5f7bcc6
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-03-04 02:02:18 -0500

    Updated index to check to make sure form is valid by intantiated new validator class. Updated payment form to include an error span for each required field to show an error is not filled out. git commit

--- index.php
+++ index.php
@@ -15,8 +15,6 @@ require("vendor/autoload.php");
 
 //Instantiate F3
 $f3 = Base::instance();
-//instantiate a new validator object
-$validate = new Validator();
 
 
 //Define a default route
@@ -32,7 +30,25 @@ $f3->route("GET /listings", function () {
 
 //creating a payment route
 //Define a default route
-$f3->route("GET /payment", function () {
+$f3->route("GET|POST /payment", function ($f3) {
+    //instantiate a new validator object
+    $validate = new Validator();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        var_dump($_POST);
+        //add to hive to make form sticky
+        $f3->set('payment',$_POST);
+        //check to ensure form is valid
+        if($validate->validForm()){
+            //reroute to thank you page
+            $f3->reroute("/thank");
+        }
+        else{
+            //Data was not valid
+            //Get errors and add to f3 hive
+            $f3->set('errors', $validate->getErrors());
+        }
+    }
     $view = new Template();
     echo $view->render("views/payment-form.html");
 });
