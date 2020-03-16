7a735eb0353e266abff404607c183d1cc1f35f6a
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-03-15 20:32:03 -0400

    changed the navbar to be a php document. Added one more link to the navbar. Touched all views pages to change the include to be a php file

--- views/admin-login.php
+++ views/admin-login.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 ?>
-<include href="views/navbar.html">
+<include href="views/navbar.php">
 
 
         <div class="jumbotron"> <!-- Header -->
